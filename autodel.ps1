ac43d6e354c721cab75f70c6c68e56d63f35d20b
mushagh <mushagh@gmail.com>
2020-09-18 15:55:35 +0800

    add dir list and user input

--- autodel.ps1
+++ autodel.ps1
@@ -8,9 +8,24 @@ $filePath = (
     "HR PAYROLL - Disk Backup\HR PAYROLL - Disk Backup"
     )
 
+# Check file directory
 Foreach ($i in $filePath) {
-    Write-Output $i
-    ForFiles /p "Y:\$i" /d -5 /c "cmd /c del @file"
+    Get-ChildItem "Y:\$i"
     Write-Output "."
     Write-Output "."
+}    
+
+# Get user input
+$usrInput = Read-Host -Prompt "Press Y to continue" 
+
+if ($usrInput -eq "Y" -OR $usrInput -eq "y") {
+    Foreach ($i in $filePath) {
+        Write-Output $i
+        ForFiles /p "Y:\$i" /d -5 /c "cmd /c del @file"
+        Write-Output "."
+        Write-Output "."
+    }
+} else {
+    exit
 }
+
